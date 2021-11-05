152f001ae14d8a69f4fd9f895bfcb3e5857b1b9a
JonathanTSilva <jonathantobias2009@hotmail.com>
2021-11-05 14:15:49 -0300

    Add all files for first version

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Sempre usar aspas simples
+  trailingComma: "all", // Para adicionar ',' no objeto que foi quebrado em várias linhas
+  arrowParens: "avoid", // Adicionar parenteses quando um arrow function tiver apenas um parâmetro
+};
